64b999853fb16b86dbedf3d17698c63e0f1cc43d
IsmaCortGtz <ismacortgtz@gmail.com>
2022-07-27 15:34:08 -0500

    First gh-pages commit

---
+++ scripts/index.js
@@ -0,0 +1,46 @@
+// Settings
+var workLimit = 25;
+var breakLimit = 5;
+var longBreakLimit = 30;
+var longBreakFrequency = 4;
+
+// HTML elements
+var counterSecond = document.getElementsByClassName("counter-seconds")[0];
+var counterMinutes = document.getElementsByClassName("counter-minutes")[0];
+var playButton = document.getElementsByClassName("play-button")[0];
+var alarmButton = document.getElementsByClassName("alarm-button")[0];
+var list = document.getElementsByClassName("row-header")[0];
+var optionButton = document.getElementsByClassName("header-option")[0];
+var optionsElements = document.getElementsByClassName("options");
+var optionsQuitButton = document.getElementsByClassName("options-quit")[0];
+
+//Setting elements
+var workLimitValue = document.getElementById("workLimite");
+var breakLimitValue = document.getElementById("breakLimite");
+var defaultAudioValue = document.getElementById("useCustomURL");
+var customUrlValue = document.getElementById("customURL");
+var audioVolumeValue = document.getElementById("alarmVolume");
+var testAlarmButton = document.getElementsByClassName("options-alarmTest-button")[0];
+var stopTestAlarmButton = document.getElementsByClassName("options-alarmTestStop-button")[0];
+var useLongBreakValue = document.getElementById("useLongBreak");
+var longBreakFrequencyValue = document.getElementById("longBreakFrecuency");
+var longBreakLimitValue = document.getElementById("longBreakLimit");
+
+// Global vars
+var timer;
+var second = 0;
+var minutes = 0;
+var working = true;
+
+var currentWorkNumber = 0;
+var endTimeDate;
+var beginTimeDate;
+
+var alarmWasPlaying = false;
+var alarmAudio = new Audio(defaultAudioURL);
+var alarmAudioTest = new Audio(defaultAudioURL);
+alarmAudio.loop = true;
+alarmAudioTest.loop  = true;
+
+updateOptionsValues();
+
---
+++ scripts/modules.js
@@ -0,0 +1,115 @@
+// Modules
+
+
+function playTime(){
+    beginTimeDate = new Date;
+    timer = setInterval( () => {
+        plusTimer();
+        refreshCounter();
+        checkIfEnd();
+    }, 1000);
+}
+
+
+function endTime(typeOfBlock){
+    playAlarm();
+    second = 0;
+    minutes = 0;
+    refreshCounter();
+    endTimeDate = new Date;
+
+    let endTimeText = intToString(endTimeDate.getHours()) + ":" + intToString(endTimeDate.getMinutes());
+    let beginTimeText = intToString(beginTimeDate.getHours()) + ":" + intToString(beginTimeDate.getMinutes());
+    working ? currentWorkNumber++ : undefined;
+    working = !working;
+
+    list.insertAdjacentHTML("afterend", `<div class="list-row">
+    <div class="list-cell">${beginTimeText}</div>
+    <div class="list-cell">${endTimeText}</div>
+    <div class="list-cell cell-type">${typeOfBlock}</div>
+</div>`);
+}
+
+
+function playAlarm(){
+    clearInterval(timer);
+    alarmAudio.play();
+    playButton.classList.add("blocked");
+    alarmButton.classList.remove("blocked");
+}
+
+
+
+function stopAlarm(){
+    playTime();
+    alarmAudio.pause();
+    alarmAudio.currentTime = 0;
+    playButton.classList.remove("blocked");
+    alarmButton.classList.add("blocked");
+}
+
+
+function plusTimer(){
+    if (second === 59){
+        second = 0;
+        minutes++;
+    }else{
+        second++;
+    }
+}
+
+
+function refreshCounter(){
+    counterSecond.innerHTML = intToString(second);
+    counterMinutes.innerHTML = intToString(minutes);
+}
+
+
+function checkIfEnd(){
+    if (working){
+        if (minutes === workLimit){
+            endTime("Work block");
+        }
+    }else{
+        if (useLongBreakValue.checked){
+            if (longBreakFrequency !== currentWorkNumber){
+                if (minutes === breakLimit){
+                    endTime("Breake block");
+                }
+            }else{
+                if (minutes === longBreakLimit){
+                    endTime("Long break block")
+                    currentWorkNumber = 0;
+                }
+            }
+        }else{
+            if (minutes === breakLimit){
+                endTime("Breake block");
+            }
+        }
+    }
+}
+
+
+function intToString(number){
+    if (number < 10){
+        return ("0" + number.toString());
+    }else if (number >= 10){
+        return number;
+    }
+}
+
+
+function updateOptionsValues(){
+    workLimit = parseInt(workLimitValue.value);
+    breakLimit = parseInt(breakLimitValue.value);
+    longBreakLimit = parseInt(longBreakLimitValue.value);
+    longBreakFrequency = parseInt(longBreakFrequencyValue.value);
+
+    if (defaultAudioValue.checked){
+        alarmAudio.src = defaultAudioURL;
+    }else{
+        alarmAudio.src = customUrlValue.value;
+    }
+    alarmAudio.volume = parseInt(audioVolumeValue.value) / 100;
+}
